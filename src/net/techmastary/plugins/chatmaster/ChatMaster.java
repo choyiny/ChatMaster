2ecc5dae965da78354471c4b2d4de8cd84520277
hkminegod <choyiny@gmail.com>
2013-09-24 15:08:15 +0800

    Prepare for v1.2.1
    Added player pings, warnings for no permission to talk/listen/added the
    functionality to deafen others (buggy)

--- src/net/techmastary/plugins/chatmaster/ChatMaster.java
+++ src/net/techmastary/plugins/chatmaster/ChatMaster.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ChatMaster extends JavaPlugin implements Listener {
 	public static boolean Silenced;
 	ChatCommand chatcmds = new ChatCommand();
+	ChatPing chatping = new ChatPing();
 	ChatEventListener chateventlistener = new ChatEventListener();
 
 	@Override
@@ -16,11 +17,12 @@ public class ChatMaster extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
-		loadConf();
+		this.saveDefaultConfig();
 		System.out.println("Enabled ChatMaster " + getDescription().getVersion() + ".");
 		getServer().getPluginManager().registerEvents(this, this);
 		getServer().getPluginManager().registerEvents(this.chateventlistener, this);
 		getServer().getPluginCommand("cm").setExecutor(this.chatcmds);
+		getServer().getPluginCommand("pping").setExecutor(this.chatping);
 		Silenced = false;
 		if (getConfig().getBoolean("metrics")) {
 			try {
@@ -30,11 +32,7 @@ public class ChatMaster extends JavaPlugin implements Listener {
 				// I'm sad.
 			}
 		} else {
-			System.out.print("[ChatMaster] Metrics was not enabled");
+			System.out.println("[ChatMaster] Metrics was not enabled");
 		}
-	}
-
-	private void loadConf() {
-		getConfig().addDefault("metrics", true);
 	}
 }
